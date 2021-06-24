bf462d98b79a78a9bf83dc2bbaac120792acdb3b
Pavel <pavlo_bsu@tut.bu>
2021-06-24 22:05:38 +0300

    Visual studio project has been created

---
+++ SampleOfTelegramBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Pavlo.SampleOfTelegramBot
+{
+    class Program
+    {
+        static TBot bot;
+        static async Task Main(string[] args)
+        {
+            bot = new TBot(@"d:\k.txt");
+            Console.ReadLine();
+        }
+    }
+}
