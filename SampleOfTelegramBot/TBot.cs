45a1b65f90fb684e9550f9e8486f2a6ef75430bd
pavlo-bsu <pavlo_bsu@tut.by>
2022-07-07 14:55:55 +0300

    modification of the class: #1. BotID is explicitly specified. #2 All extra fuctionality is removed from the class

--- SampleOfTelegramBot/TBot.cs
+++ SampleOfTelegramBot/TBot.cs
@@ -20,7 +20,7 @@ namespace Pavlo.SampleOfTelegramBot
         /// <summary>
         /// path to file with botID
         /// </summary>
-        private readonly string pathToBotID;
+        private readonly string botID = "INSERT_KEY!";
 
         /// <summary>
         /// words that wake up bot
@@ -34,46 +34,26 @@ namespace Pavlo.SampleOfTelegramBot
             get; private set;
         }
 
-        public Telegram.Bot.Types.User BotUser
-        {
-            get; 
-            private set;
-        }
-
         private Weather weatherAction;
 
-        public TBot(string pathToBotID)
+        public TBot()
         {
-            this.pathToBotID = pathToBotID;
-
-            BotOnKeywords = new List<string>() { "bot", "бот",};
+            BotOnKeywords = new List<string>() { "bot", "бот", };
 
             weatherAction = new Weather();
             BotOnKeywords.AddRange(weatherAction.BotOnKeywords);
         }
 
         /// <summary>
-        /// async initialization of the bot instance
+        /// initialization of the bot instance
         /// </summary>
         /// <returns></returns>
-        public async Task Initialize()
+        public void Initialize()
         {
-            //reading of the ID
-            string botID = await System.IO.File.ReadAllTextAsync(this.pathToBotID);
-
-            //creating of BotUser
             Bot = new Telegram.Bot.TelegramBotClient(botID);
-            BotUser = await Bot.GetMeAsync();
-
-            var cts = new CancellationTokenSource();
-            //Below the new "pooling" way instead of the "obsolete" way based on build-in events system 
-            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
-            Bot.StartReceiving(new DefaultUpdateHandler(HandleUpdateAsync, HandleErrorAsync),
-                               cts.Token);
-
         }
 
-        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        public async Task HandleUpdateAsync(Update update)
         {
             var handler = update.Type switch
             {
@@ -81,15 +61,6 @@ namespace Pavlo.SampleOfTelegramBot
                 UpdateType.CallbackQuery => CallbackQueryTask(update.CallbackQuery),
                 _ => UnknownUpdateHandlerAsync(update)
             };
-
-            try
-            {
-                await handler;
-            }
-            catch (Exception exception)
-            {
-                await HandleErrorAsync(botClient, exception, cancellationToken);
-            }
         }
 
         private async Task MessageReceivedTask(Message message)
@@ -140,21 +111,9 @@ namespace Pavlo.SampleOfTelegramBot
             }
         }
 
-        public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-        {
-            var ErrorMessage = exception switch
-            {
-                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                _ => exception.ToString()
-            };
-
-            Console.WriteLine(DateTime.Now+ " "+  ErrorMessage);
-            return Task.CompletedTask;
-        }
-
         private Task UnknownUpdateHandlerAsync(Update update)
         {
-            Console.WriteLine(DateTime.Now+$" Unhandled update type: {update.Type}");
+            Console.WriteLine($" Unhandled update type: {update.Type}");
             return Task.CompletedTask;
         }
     }
