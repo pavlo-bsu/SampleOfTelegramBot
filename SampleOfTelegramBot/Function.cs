ab3141bb985d3b38136a44e2b046d6b71ec02ab5
pavlo-bsu <pavlo_bsu@tut.by>
2022-07-13 15:35:37 +0300

    Added logic of the lambda function

--- SampleOfTelegramBot/Function.cs
+++ SampleOfTelegramBot/Function.cs
@@ -1,21 +1,48 @@
 using Amazon.Lambda.Core;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Telegram.Bot.Types;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+//[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-namespace Pavlo.SampleOfTelegramBot;
-
-public class Function
+namespace Pavlo.SampleOfTelegramBot
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+
+    public class Function
     {
-        return input.ToUpper();
+        /// <summary>
+        /// instance of the bot
+        /// </summary>
+        private TBot bot;
+
+        /// <summary>
+        /// The function processes request (each message in a chart)
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<string> FunctionHandler(JObject request, ILambdaContext context)
+        {
+            LambdaLogger.Log("REQUEST: " + JsonConvert.SerializeObject(request));
+
+            bot = new TBot();
+            bot.Initialize();
+            try
+            {
+                var updateEvent = request.ToObject<Update>();
+
+                //process the incoming update
+                await bot.HandleUpdateAsync(updateEvent);
+
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log("exception: " + e.Message);
+            }
+
+            return "lambda return";
+        }
     }
 }
